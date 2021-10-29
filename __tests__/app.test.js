021d21c49f736409be1a6cbb8aff95f2387397b0
Dylan Floyd <floyd800@gmail.com>
2021-10-29 14:00:17 -0700

    add code to complete lab requirements

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,4 +3,35 @@ const app = require('../lib/app');
 
 describe('app routes', () => {
 
+  test('GET / responds with hi', async() => {
+    const resp = await request(app).get('/');
+    expect(resp.text).toEqual('hi');
+  });
+
+  test('POST /echo responses with the request body', async() => {
+    const resp = await request(app)
+      .post('/echo')
+      .send('bob');
+
+    expect(resp.text).toEqual('bob');
+  });
+
+  test('GET /red gets a big red', async() => {
+    const resp = await request(app)
+      .get('/red');
+    expect(resp.text).toEqual('<h1>red</h1>');
+  });
+
+  test('GET /green gets a big green', async() => {
+    const resp = await request(app)
+      .get('/green');
+    expect(resp.text).toEqual('<h1>green</h1>');
+  });
+
+  test('GET /blue gets a big blue', async() => {
+    const resp = await request(app)
+      .get('/blue');
+    expect(resp.text).toEqual('<h1>blue</h1>');
+  });
+
 });
